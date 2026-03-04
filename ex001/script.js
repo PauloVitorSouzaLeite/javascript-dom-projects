ec1ad4122f816cf34ab79d6148bdbc79b98ac5e6
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-03-04 13:20:37 -0300

    site

---
+++ ex001/script.js
@@ -0,0 +1,6 @@
+var a = document.getElementById("botao")
+a.addEventListener("click", clicar)
+function clicar() {
+    var info = document.getElementById("informacoes")
+    info.style.display = "block"
+}
