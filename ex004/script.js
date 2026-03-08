e81a1f9dc491bd3b51a68d3d9958260b1f8b7f1a
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-03-07 23:23:28 -0300

    colocando imagens como links

--- ex004/script.js
+++ ex004/script.js
@@ -1,10 +1,36 @@
-var botao = document.getElementById("cliques")
-botao.addEventListener("click", executar)
-function executar() {
-    var bloco = document.getElementById("resultado")
-    if (bloco.style.backgroundColor == "white") {
-        bloco.style.backgroundColor = "red"    
+var mostrar = document.getElementById("botao")
+mostrar.addEventListener("click", mostrar_frases)
+var image = document.getElementById("menu")
+image.addEventListener("click", exibir)
+function mostrar_frases() {
+    var frase = document.getElementById("frases")
+    var numeros = Math.floor(Math.random() * 4) + 1  
+    console.log(numeros)
+    if (frase.style.display == "none") {
+        frase.style.display = "block"
     } else {
-        bloco.style.backgroundColor = "white"
+        frase.style.display = "none"
+    }
+    switch (numeros) {
+        case 1:
+            frase.textContent = "Disciplina é fazer o que precisa ser feito, mesmo quando você não quer."
+            break
+        case 2:
+            frase.textContent = "Pequenos progressos todos os dias constroem grandes resultados."
+            break
+        case 3:
+            frase.textContent = "O sucesso é a soma de esforço repetido diariamente."
+            break
+        case 4:
+            frase.textContent = "Seu futuro depende das decisões que você toma hoje."
+            break
+    }
+}
+function exibir() {
+    var icones = document.getElementById("navegar")
+    if (icones.style.display == "none") {
+        icones.style.display = "block"
+    } else {
+        icones.style.display = "none"
     }
 }
