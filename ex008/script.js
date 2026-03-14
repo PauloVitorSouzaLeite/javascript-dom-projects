a6a136f7a7376e2a35dbd889a61030b87085b787
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-03-14 16:47:29 -0300

    Site interativo

---
+++ ex008/script.js
@@ -0,0 +1,23 @@
+function executar() {
+    var corpo = document.querySelector("body")
+    var mostrar = document.querySelector("#resultado")
+    var imagem = document.createElement("img")
+    var bloco_img = document.querySelector("#imgn")
+    var horas = new Date().getHours()
+    var minutos = new Date().getMinutes()
+    mostrar.innerHTML = ""
+    bloco_img.innerHTML = ""
+    if (minutos < 10) {
+        minutos =  "0" + minutos
+    }
+    if (horas < 18 && horas >= 5) {
+        corpo.style.backgroundColor = "burlywood"
+        imagem.setAttribute("src", "imagens/sol.png")
+    } else {
+        corpo.style.backgroundColor = "dimgrey"
+        imagem.setAttribute("src", "imagens/lua.png")
+    }
+    mostrar.textContent += `${horas}:${minutos}`
+    bloco_img.append(imagem)
+}
+setInterval(executar,1000)
