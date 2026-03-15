3ad9a9c554942185ee69363d3ce1bd1d78933384
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-03-14 23:43:44 -0300

    site interativo

--- ex008/script.js
+++ ex008/script.js
@@ -5,10 +5,14 @@ function executar() {
     var bloco_img = document.querySelector("#imgn")
     var horas = new Date().getHours()
     var minutos = new Date().getMinutes()
+    var segundos = new Date().getSeconds()
     mostrar.innerHTML = ""
     bloco_img.innerHTML = ""
     if (minutos < 10) {
         minutos =  "0" + minutos
+    }
+    if (segundos < 10) {
+        segundos = "0" + segundos
     }
     if (horas < 18 && horas >= 5) {
         corpo.style.backgroundColor = "burlywood"
@@ -17,7 +21,7 @@ function executar() {
         corpo.style.backgroundColor = "dimgrey"
         imagem.setAttribute("src", "imagens/lua.png")
     }
-    mostrar.textContent += `${horas}:${minutos}`
+    mostrar.textContent += `${horas}:${minutos}:${segundos}`
     bloco_img.append(imagem)
 }
 setInterval(executar,1000)
