202385c2f0fd7549110508f167f6045ca49a6259
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-04-06 23:12:16 -0300

    commit

--- ex011/script.js
+++
@@ -1,9 +0,0 @@
-let bloco = document.querySelector("#resultado")
-bloco.innerHTML = 0
-let button = document.querySelector("#button")
-button.addEventListener("click",executar)
-function executar() {
-    let num = Number(bloco.innerHTML)
-    num++
-    bloco.innerHTML = num
-}
