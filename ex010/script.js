9307be8ec2ca616c9e6021e009c48564201be088
Paulo Vitor Souza Leite <vitorsousaleite86@gmail.com>
2026-04-07 22:48:50 -0300

    commit

--- ex010/script.js
+++ ex010/script.js
@@ -1,9 +1,21 @@
-let bloco = document.querySelector("#resultado")
-bloco.innerHTML = 0
-let button = document.querySelector("#button")
-button.addEventListener("click",executar)
-function executar() {
-    let num = Number(bloco.innerHTML)
+let bloco = document.querySelector("#number-block")
+let aumentar = document.querySelector("#more-button")
+let diminuir = document.querySelector("#less-button")
+let resetar = document.querySelector("#restart-button")
+aumentar.addEventListener("click", adicionar)
+diminuir.addEventListener("click", subtrair)
+resetar.addEventListener("click", restart) 
+bloco.innerHTML = "0"
+let num = Number(bloco.innerHTML)
+function adicionar() {
     num++
+    bloco.innerHTML = num
+}
+function subtrair() {
+    num--
+    bloco.innerHTML = num
+}
+function restart() {
+    num = 0
     bloco.innerHTML = num
 }
